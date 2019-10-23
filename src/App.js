38284514a5b3818e686bc24666c7dfb6347f5534
Sayan Mukherjee <hello@sayanmukherjee.com>
2019-10-23 11:31:20 +0530

    updating frontend files

--- src/App.js
+++ src/App.js
@@ -45,8 +45,25 @@ class App extends Component{
       imageUrl:'',
       box:{},
       route: 'signin',
-      isSignedIn:false
+      isSignedIn:false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+       id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -75,13 +92,28 @@ class App extends Component{
       app.models.predict(
           Clarifai.FACE_DETECT_MODEL,
           this.state.input)
-        .then(response => this.displayFaceBox (this.calculateFaceLocation(response)))   
+        .then(response => {
+           if (response) {
+              fetch('http://localhost:3000/image', {
+                method: 'put',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                  id: this.state.user.id
+                })
+              })
+              .then(response => response.json())
+              .then(count => {
+                this.setState(Object.assign(this.state.user, { entries: count}))
+              })
+            }
+          this.displayFaceBox(this.calculateFaceLocation(response))
+        })
         .catch(err => console.log(err));
   }
 
   onRouteChange = (route) => {
     if(route === 'signout'){
-      this.setState({isSignedIn:false})
+      this.setState({isSignedIn:false, route: 'signin'})
     } else if(route === 'home'){
       this.setState({isSignedIn: true})
     }
@@ -99,14 +131,20 @@ class App extends Component{
             <Logo />
             { route === 'home' ? 
               <div>
-                <Rank />
-                <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+                <Rank 
+                  name={this.state.user.name} 
+                  entries={this.state.user.entries}
+                />
+                <ImageLinkForm 
+                  onInputChange={this.onInputChange} 
+                  onButtonSubmit={this.onButtonSubmit}
+                  />
                 <FaceRecognition box={box} imageUrl={imageUrl}/>
               </div>
             : (
                 route === 'signin' 
-                ? <Signin onRouteChange={this.onRouteChange}/>
-                : <Register onRouteChange={this.onRouteChange}/>
+                ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+                : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
               )
             }
         </div>
