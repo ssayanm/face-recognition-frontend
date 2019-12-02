98c0b92ce8808d1550e8f47545d42cbba1c04952
Sayan Mukherjee <hello@sayanmukherjee.com>
2019-12-02 15:52:58 +0530

    logo centered and updated

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Tilt from "react-tilt";
-import brain from "./brain.png";
+import brain from "./brain.jpg";
 import "./Logo.css";
 
 const Logo = () => {
   return (
-    <div className="ma4 mt0 tc">
+    <div className="ma4 mt0 center">
       <Tilt
         className="Tilt br2 shadow-2"
         options={{ max: 55 }}
